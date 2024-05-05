1a0dfc05fc07c9aff6a3d6712fa9c31916cfb001
Majkyy <marian.sebora9@gmail.com>
2024-05-05 19:59:19 +0200

    Initial commit

---
+++ templates/partials/header.php
@@ -0,0 +1,49 @@
+
+<?php
+  require('../_inc/functions.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php 'Moj web | '. (basename($_SERVER["SCRIPT_NAME"], '.php'));?></title>
+    <?php
+      //add_stylesheet();
+      $page_name = basename($_SERVER["SCRIPT_NAME"], '.php');
+      $page_object = new Page($page_name);
+      $page_object->add_stylesheet();
+    ?>
+</head>
+<body>
+    <header class="container main-header">
+        <div>
+          <a href="home.php">
+            <img src="../assets/img/logo.png" height="40">
+          </a>
+        </div>
+      <nav class="main-nav">
+      <ul class="main-menu" id="main-menu">
+        <?php
+           $pages = array('Domov'=>'home.php',
+                'Portfólio'=>'portfolio.php',
+                'Q&A'=>'qna.php',
+                'Kontakt'=>'kontakt.php'  
+           );
+           //echo(generate_menu($pages));
+           //$menu_object = new Menu($pages);
+           //echo($menu_object->generate_menu());
+           if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true){
+              $pages['Odhlasiť'] = 'logout.php';
+           }
+
+          $menu_object  = new Menu($pages);
+          echo($menu_object->generate_menu());
+        ?>
+       
+        </ul>
+        <a class="hamburger" id="hamburger">
+            <i class="fa fa-bars"></i>
+        </a>
+      </nav>
+    </header>
---
+++ templates/thankyou.php
@@ -0,0 +1,23 @@
+<?php
+include('partials/header.php');
+?> 
+<main>
+      <?php include('partials/banner.php');?>
+      <section class="container">
+        <div class="row">
+          <div class="col-100 text-center">
+              <h2>Ďakujeme za vyplnenie formulára</h2>
+              <?php
+                //print_r($_POST);
+                //$conn = db_connection();
+                $contact_object = new Contact();
+                $contact_object->insert();
+              ?>
+          </div>
+        </div>
+      </section>
+    </main>
+    
+<?php
+    include_once('partials/footer.php')
+?> 
